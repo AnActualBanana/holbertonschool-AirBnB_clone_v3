95c3c6d27da15bb206ee0ad96ec34e33206378e9
AnActualBanana <loganmcclain59@gmail.com>
2022-11-21 14:32:54 -0800

    stats endpoint method, task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,19 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
 	"""show the status function"""
 	return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def stats():
+    """return number of stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
